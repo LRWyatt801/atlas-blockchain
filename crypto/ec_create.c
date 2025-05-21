e7018d27f41be6bcc8249223ae833685d038371d
LRWyatt801 <loganwyatt1995@gmail.com>
2025-05-20 20:14:13 -0500

    push ec_create

---
+++ crypto/ec_create.c
@@ -0,0 +1,26 @@
+#include <openssl/obj_mac.h>
+#include "hblk_crypto.h"
+
+#define EC_CURVE_NID NID_secp256k1
+
+/**
+* ec_create - creates a new EC key pair
+*
+* Return: pointer to a EC_KEY structure on success, otherwise NULL
+*/
+
+EC_KEY *ec_create(void)
+{
+	EC_KEY *key = EC_KEY_new_by_curve_name(EC_CURVE_NID);
+
+	if (!key)
+		return (NULL);
+
+	if (EC_KEY_generate_key(key) != 1)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+
+	return (key);
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -2,12 +2,13 @@
 #define HBLK_CRYPTO_H
 
 #include <openssl/sha.h>
+#include <openssl/ec.h>
 
 /***************************** PROVIDED PROTOTYPES ***************************/
 
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH]);
-
+EC_KEY *ec_create(void);
 
 /*****************************************************************************/
 
