6d7bff590f22ba87636d0f8a91c3fcdc6eb6dfe2
lrwyatt801 <loganwyatt1995@gmail.com>
2025-05-27 20:59:29 -0500

    push ec_sign.c:

---
+++ crypto/ec_sign.c
@@ -0,0 +1,28 @@
+#include "hblk_crypto.h"
+
+/**
+* ec_sign - signs a given set of bytes using a given EC_KEY private key
+* @key: points the EC_KEY structure containing the private key to be used
+* @msg: points to a string of characters of msglen
+* @msglen: len of msg
+* @sig: holds the address of where to store the signature
+*
+* Return: pointer to the signature buffer on success, otherwise NULL
+*/
+
+uint8_t *ec_sign(EC_KEY const *key,
+		 uint8_t const *msg,
+		 size_t msglen, sig_t *sig)
+{
+	if (!key || !msg)
+		return (NULL);
+
+	if (!ECDSA_sign(0,
+		 	msg,
+		 	msglen,
+		 	sig->sig,
+		 	(unsigned int *)&sig->len,
+		 	(EC_KEY *)key))
+		return (NULL);
+	return (sig->sig);
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -4,9 +4,21 @@
 #include <openssl/sha.h>
 #include <openssl/ec.h>
 #include <openssl/obj_mac.h>
+#include <openssl/ecdsa.h>
 
 #define EC_PUB_LEN 65 /* EC_KEY public key octet str len - uncompressed */
 #define EC_CURVE NID_secp256k1 /* needed to fix checker compile error */
+
+/**
+ * struct sig_s - struct to hold sig
+ * @sig: pointer to signature buffer
+ * @len: length of signature
+ */
+
+typedef struct {
+	uint8_t *sig;
+	size_t len;
+} sig_t;
 
 /***************************** PROVIDED PROTOTYPES ***************************/
 
@@ -17,6 +29,10 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 EC_KEY *ec_load(char const *folder);
+uint8_t *ec_sign(EC_KEY const *key,
+		 uint8_t const *msg,
+		 size_t msglen,
+		 sig_t *sig);
 
 /*****************************************************************************/
 
