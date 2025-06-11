9b74226dfe555570884f35bbeb1861affa0a78bc
lrwyatt801 <loganwyatt1995@gmail.com>
2025-06-10 20:27:56 -0500

    update macros

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -9,6 +9,12 @@
 
 #include <llist.h>
 
+#define HOLBERTON_HASH \
+	"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
+	"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
+#define HOLBERTON_DATA "Holberton School"
+#define GEN_TIMESTAMP 1537578000
+
 #define BLOCKCHAIN_DATA_MAX 1024
 
 /*********************************PROVIDED STRUCTS****************************/
