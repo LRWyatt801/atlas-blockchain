cc7d7a5fe037da3b1d661bec7c76dbccd5ca26f4
lrwyatt801 <loganwyatt1995@gmail.com>
2025-06-14 19:20:20 -0500

    push block_mine

---
+++ blockchain/v0.2/block_mine.c
@@ -0,0 +1,29 @@
+#include "blockchain.h"
+#include <openssl/sha.h>
+
+/**
+* block_mine - mines a block in order to insert it in the Blockchain
+* @block: points to the block to be mined
+*
+* Return: n/a
+*/
+
+void block_mine(block_t *block)
+{
+	uint8_t hash[SHA256_DIGEST_LENGTH];
+	uint64_t nonce;
+
+	while (1)
+	{
+		block->info.nonce = nonce;
+		block_hash(block, hash);
+
+		if (hash_matches_difficulty(hash, block->info.difficulty))
+		{
+			memcpy(block->hash, hash, SHA256_DIGEST_LENGTH);
+			return;
+		}
+		nonce++;
+	}
+
+}
