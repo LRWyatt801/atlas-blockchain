fc66855af5d26306d3e3ebe1f90600adacfdf867
lrwyatt801 <loganwyatt1995@gmail.com>
2025-06-29 20:56:43 -0500

    pushing transaction_hash, not completed

---
+++ blockchain/v0.3/transaction/transaction_hash.c
@@ -0,0 +1,76 @@
+#include "transaction.h"
+#include <stddef.h>
+
+#define INPUT_SIZE(cnt) ((cnt) * 96)
+#define OUTPUT_SIZE(cnt) ((cnt) * 32)
+
+static int input_cpy(llist_node_t, unsigned int, void *);
+static int output_cpy(llist_node_t, unsigned int, void *);
+
+/**
+* transaction_hash - computes the ID(hash) of a transaction
+* @transaction: points to the transaction to compute the hash of
+* @hash_buf: buffer in which to store the computed hash
+*
+* Return: pointer to hash_buf
+*/
+
+uint8_t *transaction_hash(transaction_t const *transaction, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+	uint8_t *tx_buffer, *save_buff;
+	int input_cnt, output_cnt;
+
+	if (!transaction)
+		return (NULL);
+
+	input_cnt = llist_size(transaction->inputs);
+	output_cnt = llist_size(transaction->outputs);
+
+	tx_buffer = malloc(sizeof(INPUT_SIZE(input_cnt) + OUTPUT_SIZE(output_cnt)));
+	if (!tx_buffer)
+		return (NULL);
+	save_buff = tx_buffer;
+	llist_for_each(transaction->inputs, input_cpy, tx_buffer);
+	llist_for_each(transaction->outputs, output_cpy, tx_buffer);
+
+	SHA256(save_buff, sizeof(tx_buffer), hash_buf);
+	return (hash_buf);
+}
+
+/**
+* input_cpy - copys transaction inputs to buffer
+* @buffer: buffer to copy to
+*
+* Return: NULL
+*/
+
+static int input_cpy(llist_node_t node, unsigned int index, void *buffer)
+{
+	(void)index;
+	if (!buffer)
+		return (-1);
+
+	memcpy(buffer, node, 96);
+
+	return (0);
+}
+
+/**
+* output_cpy - copys transaction output to buffer
+* @node: node to copy
+* @index: index of node NOTE this value is not used
+* @buffer: buffer to copy to
+*
+* Return: 0 on success, -1 on fialure
+*/
+
+static int output_cpy(llist_node_t node, unsigned int index, void *buffer)
+{
+	(void)index;
+	if (!node || !index || !buffer)
+		return (-1);
+
+	memcpy(buffer, (tx_out_t *)offsetof(tx_out_t, hash), 32);
+
+	return (0);
+}
