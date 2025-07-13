56a228775ac4e56ec5b8d554eb25618d57b574b7
lrwyatt801 <loganwyatt1995@gmail.com>
2025-07-13 14:36:03 -0500

    Updated block_hash to hash transactions

--- blockchain/v0.3/block_hash.c
+++ blockchain/v0.3/block_hash.c
@@ -1,5 +1,8 @@
 #include "blockchain.h"
-#include "../../crypto/hblk_crypto.h"
+
+#define TX_SIZE(num_tx) ((num_tx) * SHA256_DIGEST_LENGTH)
+
+static int copy_tx(llist_node_t, unsigned int, void *);
 
 /**
 * block_hash - computes the hash of a block
@@ -12,11 +15,37 @@
 uint8_t *block_hash(block_t const *block,
 		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
+	uint8_t *buff;
+	size_t num_tx = 0, block_size, buff_size;
+
 	if (!block || !hash_buf)
 		return (NULL);
 
+	if (block->transactions)
+		num_tx = llist_size(block->transactions);
 
-	return (sha256((int8_t *)block,
-		       sizeof(block->info) + block->data.len,
-		       hash_buf));
+	block_size = sizeof(block->info) + block->data.len;
+	buff_size = block_size + TX_SIZE(num_tx); 
+
+	memcpy(buff, block, block_size);
+	llist_for_each(block->transactions, copy_tx, buff + block_size);
+
+	return (SHA256(buff, buff_size, hash_buf));
+}
+
+/**
+* copy_tx - copy the has of transactions
+* @node: current node
+* @index: index of node
+* @buff: buffer to copy too
+*
+* Return: 0 on success, otherwise 1
+*/
+
+static int copy_tx(llist_node_t node, unsigned int index, void *buff)
+{
+	memcpy(
+		(uint8_t *)buff + index * SHA256_DIGEST_LENGTH,
+		((transaction_t *)node)->id, SHA256_DIGEST_LENGTH);
+	return (0);
 }
